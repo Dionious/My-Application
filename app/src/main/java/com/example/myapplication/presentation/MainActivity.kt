538026ccfa42ae6cbb6be6a4f1d8aa69c98b2be6
Denis <denistimofeev@myrambler.ru>
2021-07-08 21:07:09 +0300

    Third commit realize login and welcome with navigation component

--- app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
@@ -8,16 +8,8 @@ import androidx.fragment.app.FragmentTransaction
 import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
-    lateinit var fragment: Fragment
-    lateinit var fragmentManager: FragmentManager
-    lateinit var fragmentTransaction: FragmentTransaction
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fragment = LoginFragment()
-        fragmentManager = supportFragmentManager
-        fragmentTransaction = fragmentManager.beginTransaction()
-       fragmentTransaction.add(R.id.container,fragment)
-        fragmentTransaction.commit()
     }
 }
